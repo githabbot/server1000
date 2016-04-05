ddbd46408aac32fd1969d27bdab3513aca8bfcdc
githabbot <m.githabbot@gmail.com>
2016-04-05 19:52:29 +0430

    Update support.lua

--- plugins/support.lua
+++ plugins/support.lua
@@ -6,7 +6,7 @@ local function callback(extra, success, result)
 end
 
 local function run(msg, matches)
-  local user = 158990680
+  local user = 195473011
 
   if matches[1] == "support" then
     user = 'user#id'..user
